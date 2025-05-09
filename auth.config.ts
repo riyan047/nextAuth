08fc2a1f793f21884e470425771824c6195b3066
Riyan <griyan115@gmail.com>
2025-05-09 19:57:24 +0530

    Login func added

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,25 @@
-import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validField = LoginSchema.safeParse(credentials);
+        if (validField.success) {
+          const { email, password } = validField.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
