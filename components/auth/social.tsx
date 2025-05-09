ccf2e58fc2540a571f4a135fbd42df8821ad0579
Riyan <griyan115@gmail.com>
2025-05-09 10:08:50 +0530

    fixed social comp

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -4,9 +4,7 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 
-export const Social = ({
-
-}) => {
+export const Social = () => {
     return (
         <div className="flex items-center w-full gap-x-2">
             <Button
