a07f46f22513253d8ec867d8e18fdced2dea87cd
Riyan <griyan115@gmail.com>
2025-05-10 20:12:45 +0530

    loginAction fixed

--- actions/login.ts
+++ actions/login.ts
@@ -5,12 +5,19 @@ import { LoginSchema } from "@/schemas";
 import { AuthError } from "next-auth";
 import * as z from "zod";
 
-export const login = async (values: z.infer<typeof LoginSchema>) => {
+type LoginResponse =
+  | { success: string; error?: undefined }
+  | { error: string; success?: undefined };
+
+export const login = async (
+  values: z.infer<typeof LoginSchema>
+): Promise<LoginResponse> => {
   const validatedFields = LoginSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Invalid credentials!" };
   }
+
   const { email, password } = validatedFields.data;
 
   try {
@@ -19,6 +26,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
+
+    return { success: "Login successful!" };
   } catch (error) {
     if (error instanceof AuthError) {
       if (error.message === "CredentialsSignIn") {
@@ -27,6 +36,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { error: "Something went wrong" };
       }
     }
+
     throw error;
   }
 };
+
