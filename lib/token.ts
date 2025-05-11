5fc8f18c2bfd849c17b1b3a6d80210f2756ceca0
Riyan <griyan115@gmail.com>
2025-05-11 19:36:12 +0530

    generating verification token

---
+++ lib/token.ts
@@ -0,0 +1,28 @@
+import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { db } from "./db";
+import { v4 as uuid } from "uuid";
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuid();
+  const expires = new Date(new Date().getTime() + 3600 * 1000); //the token has a validity of 1 hr.
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const verificationToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return verificationToken;
+};
