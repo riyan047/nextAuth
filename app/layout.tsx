8f8db036ed5463e50d6f937383d72fcb4f99f446
Riyan <griyan115@gmail.com>
2025-05-14 18:31:22 +0530

    admin permission/api routes handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default async function RootLayout({
     <SessionProvider session={session}>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <Toaster />
           {children}
         </body>
       </html>
