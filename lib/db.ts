d398d4e439d215bb4a4c209149e80c8a89b5e038
Riyan <griyan115@gmail.com>
2025-05-16 23:35:40 +0530

    error fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,16 @@
+
 import { PrismaClient } from "@prisma/client";
 
+/* eslint-disable no-var */
 declare global {
   var prisma: PrismaClient | undefined;
 }
+/* eslint-enable no-var */
 
-export const db = globalThis.prisma || new PrismaClient();
+const prisma = globalThis.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export const db = prisma;
