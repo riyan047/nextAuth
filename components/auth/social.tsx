7ab1990f844221b3ac13d3602c539b611f8412ee
Riyan <griyan115@gmail.com>
2025-05-16 22:11:42 +0530

    imp

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -5,13 +5,17 @@ import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useSearchParams } from "next/navigation";
+
 
 export const Social = () => {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("callbackUrl")
 
     const onClick = (provider: "google" | "github") => {
         //NOTE SIGNIN IS IMPORTED FROM NEXT-AUTH/REACT WHICH IS A CLIENT SIDE CODE IF WE IMPORT FROM @/AUTH IT IS SERVER COMPO
         signIn(provider, {
-            callbackUrl: DEFAULT_LOGIN_REDIRECT
+            callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT
         })
     }
     return (
